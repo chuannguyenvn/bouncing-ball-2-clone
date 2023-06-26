806f386266e9dfdbca869c556ec7a7271f08c539
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-26 15:19:38 +0700

    feat: object pool

---
+++ src/utilities/ObjectPool.ts
@@ -0,0 +1,50 @@
+﻿class ObjectPool<T extends IPoolable>
+{
+    private readonly pool: T[] = []
+    private readonly createFunction: () => T
+
+    constructor(initialSize: number, createFunction: () => T) {
+        this.createFunction = createFunction
+
+        for (let i = 0; i < initialSize; i++)
+        {
+            this.createNewInstance(false)
+        }
+    }
+
+    private createNewInstance(startAsAwake: boolean): T {
+        const instance = this.createFunction()
+        instance.isAwake = startAsAwake
+        if (!startAsAwake) instance.sleep()
+        this.pool.push(instance)
+        return instance
+    }
+
+    private getInstance(): T {
+        for (let i = 0; i < this.pool.length; i++)
+        {
+            if (this.pool[i].isAwake) continue
+            this.pool[i].wake()
+            this.pool[i].isAwake = true
+            return this.pool[i]
+        }
+
+        return this.createNewInstance(true)
+    }
+
+    private returnInstance(instance: T) {
+        instance.sleep()
+        instance.isAwake = false
+    }
+}
+
+interface IPoolable
+{
+    isAwake: boolean
+
+    wake(): void
+
+    sleep(): void
+}
+
+export {ObjectPool, IPoolable}
