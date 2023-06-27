b6bc08d5587add725e5042affa41e3434eff472f
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-27 11:22:52 +0700

    feat: eased platform heights

--- src/objects/PlatformSpawner.ts
+++ src/objects/PlatformSpawner.ts
@@ -2,17 +2,23 @@
 import PlayScene from "../scenes/PlayScene"
 import {ParamGameEvent} from "../utilities/Event"
 import Constants from "../configs/Constants"
+import IUpdatable from "../interfaces/IUpdatable"
 
 class PlatformSpawner
 {
     public touchedPlatformIndex: ParamGameEvent<number> = new ParamGameEvent<number>()
 
+    private playScene: PlayScene
+
     private platforms: Platform[] = []
     private currentPlatformIndex: number = 0
     private farthestPlatformXPosition: number = -200
     private currentWidth: number = 100
+    private currentHeight: number = 600
 
     constructor(scene: PlayScene) {
+        this.playScene = scene
+
         for (let i = 0; i < Constants.PLATFORM_POOL_SIZE; i++)
         {
             const platform = new Platform(scene)
@@ -36,13 +42,29 @@ class PlatformSpawner
 
             this.currentWidth = Math.max(this.currentWidth - 10, 20)
         })
+        
+        this.easePlatformHeight()
     }
 
     public placeNextPlatform() {
         this.currentPlatformIndex = (this.currentPlatformIndex + 1) % Constants.PLATFORM_POOL_SIZE
         const platform = this.platforms[this.currentPlatformIndex]
-        platform.setup(this.farthestPlatformXPosition + 200 + (Math.random() * 2 - 1) * 5, Math.random() * 200 + 500, this.currentWidth)
+        platform.setup(this.farthestPlatformXPosition + 200 + (Math.random() * 2 - 1) * 5, this.currentHeight, this.currentWidth)
         this.farthestPlatformXPosition += 200
+        console.log(this.currentHeight)
+    }
+
+    private easePlatformHeight(): void {
+        this.playScene.tweens.addCounter({
+            from: this.currentHeight,
+            to: Math.random() * 200 + 400,
+            ease: 'Sine.inout',
+            duration: (Math.random() * 5 + 10) * 500,
+            onUpdate: (tween) => {
+                this.currentHeight = tween.getValue()
+            },
+            onComplete: () => this.easePlatformHeight(),
+        })
     }
 }
 
