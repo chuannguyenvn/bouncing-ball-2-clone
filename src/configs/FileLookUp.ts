c617fdb585a00d314e58cb3148bbf1521a88f588
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-29 17:56:28 +0700

    other: build

--- src/configs/FileLookUp.ts
+++ src/configs/FileLookUp.ts
@@ -1,16 +1,16 @@
 ﻿import SpriteKey from "./SpriteKey"
 
 const FileLookUp = {
-    [SpriteKey.SQUARE]: '../../assets/images/Square.png',
-    [SpriteKey.GRADIENT]: '../../assets/images/Gradient.png',
-    [SpriteKey.BALL_DEFAULT]: '../../assets/images/Ball_Default.png',
-    [SpriteKey.BALL_HAPPY]: '../../assets/images/Ball_happy.png',
-    [SpriteKey.BALL_CHIP]: '../../assets/images/Ball_chip.png',
-    [SpriteKey.BALL_LEMON]: '../../assets/images/Ball_lemon.png',
-    [SpriteKey.BALL_YINGYANG]: '../../assets/images/Ball_yingyang.png',
-    [SpriteKey.BUTTON_IDLE]: '../../assets/images/Button_idle.png',
-    [SpriteKey.BUTTON_CLICKED]: '../../assets/images/Button_clicked.png',
-    [SpriteKey.GEM]: '../../assets/images/Gem.png',
+    [SpriteKey.SQUARE]: './assets/images/Square.png',
+    [SpriteKey.GRADIENT]: './assets/images/Gradient.png',
+    [SpriteKey.BALL_DEFAULT]: './assets/images/Ball_Default.png',
+    [SpriteKey.BALL_HAPPY]: './assets/images/Ball_happy.png',
+    [SpriteKey.BALL_CHIP]: './assets/images/Ball_chip.png',
+    [SpriteKey.BALL_LEMON]: './assets/images/Ball_lemon.png',
+    [SpriteKey.BALL_YINGYANG]: './assets/images/Ball_yingyang.png',
+    [SpriteKey.BUTTON_IDLE]: './assets/images/Button_idle.png',
+    [SpriteKey.BUTTON_CLICKED]: './assets/images/Button_clicked.png',
+    [SpriteKey.GEM]: './assets/images/Gem.png',
 }
 
 export default FileLookUp
