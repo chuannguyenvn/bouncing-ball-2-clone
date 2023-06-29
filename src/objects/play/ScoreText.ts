6210a73d931ff00f3967ac8b0cf62f7b1bca266a
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-29 11:54:51 +0700

    refactor: font

--- src/objects/play/ScoreText.ts
+++ src/objects/play/ScoreText.ts
@@ -6,12 +6,12 @@ class ScoreText extends Phaser.GameObjects.Text
     private playScene: PlayScene
 
     constructor(scene: PlayScene) {
-        super(scene, scene.scale.width / 2, 100, '0', {font: '100px open sans', color: '#aaaaaa'})
+        super(scene, scene.scale.width / 2, 100, '0', {font: '150px calibri', color: '#dddddd'})
         this.scene.add.existing(this)
         this.playScene = scene
 
         this.setScrollFactor(0)
-        this.setOrigin(0.5)
+        this.setOrigin(0.5, 0)
 
         this.playScene.scoreChanged.subscribe((score) => this.text = score.toString())
     }
